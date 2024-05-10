323f866abefd748d6f852f0dd4c5d273fc54e7b6
biaholi <106541018+biaholi@users.noreply.github.com>
2024-05-10 11:38:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const botoes = document.querySelectorAll(".botao" );
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
         
       for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
       }
-        
+
         botoes[i].classList.add("ativo");
-    }
-    console.log();
-}
+        textos[i].classList.add("ativo");
+     }
+  }
+
+  const contadores = document.querySelectorAll(".contador");
+  const tempoObjetivo1 = new Date("2024-10-05t00:00:00");
+  
+  contadores[0].textContent = calculaTempo(tempoObjetivo1);
+  
+  function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    
+
+  }
